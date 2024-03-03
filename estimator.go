67f20f8c7ecca2aa0ff1cb08d2412e732928abcf
Ryan Fowler <ryan.fowler19@gmail.com>
2024-03-03 11:27:16 -0800

    Use different default sizes for Rate and Estimator

--- estimator.go
+++ estimator.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Ryan Fowler
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package limits
 
 import (
@@ -11,7 +25,7 @@ const (
 	// DefaultHashes represents the default value for the number of hashes.
 	DefaultHashes = 4
 	// DefaultSlots represents the default value for the number of slots.
-	DefaultSlots = 1024
+	DefaultSlots = 8192
 )
 
 // Key represents the type constraint for keys.
--- rate.go
+++ rate.go
@@ -1,8 +1,29 @@
+// Copyright 2024 Ryan Fowler
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package limits
 
 import (
 	"sync/atomic"
 	"time"
+)
+
+const (
+	// DefaultRateHashes represents the default value for the number of hashes.
+	DefaultRateHashes = 4
+	// DefaultRateSlots represents the default value for the number of slots.
+	DefaultRateSlots = 1024
 )
 
 // Rate is a probabilistic rate estimator over a given interval. Internally, it
@@ -21,7 +42,7 @@ type Rate[K Key] struct {
 // NewRate returns a new Rate instance using the provided interval and default
 // sizes. NewRate panics if interval is smaller than 1 millisecond.
 func NewRate[K Key](interval time.Duration) *Rate[K] {
-	return NewRateWithSize[K](interval, DefaultHashes, DefaultSlots)
+	return NewRateWithSize[K](interval, DefaultRateHashes, DefaultRateSlots)
 }
 
 // NewRateWithSize returns a new Rate instance using the provided interval and
